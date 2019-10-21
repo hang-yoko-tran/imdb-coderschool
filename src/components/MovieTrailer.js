6173827a27ead760e7e0680aaffa8624377f2838
thi131190 <thi131190@gmail.com>
2019-10-21 14:34:56 +0700

    Style MovieTrailer

--- src/components/MovieTrailer.js
+++ src/components/MovieTrailer.js
@@ -3,14 +3,29 @@ import ReactModal from 'react-modal'
 import YouTube from '@u-wave/react-youtube'
 
 const MovieTrailer = props => {
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)'
+    }
+  }
   return (
     <ReactModal
       isOpen={props.showModal}
+      style={customStyles}
       contentLabel='onRequestClose Example'
       onRequestClose={props.handleCloseModal}
     >
-      <YouTube video={props.trailerKey} autoplay />
-      <button onClick={props.handleCloseModal}>Close Modal</button>
+      <YouTube
+        video={props.trailerKey}
+        height='480'
+        width='720'
+        autoplay={false}
+      />
     </ReactModal>
   )
 }
